2581d62926c6326ea4d46a6f73b81334c4230b2e
amann013 <coolaman4554@gmail.com>
2022-06-28 18:07:11 +0530

    initial commit

---
+++ client/src/baseUrl.js
@@ -0,0 +1,2 @@
+
+export const baseUrl="https://librmern.herokuapp.com/api/";
---
+++ client/src/components/FooterComponent.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+
+class Footer extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+        }
+    }
+
+render(){
+    return(
+                    <p className="footer" align="center">© Copyright 2022 NIT JAMSHEDPUR </p>
+          );
+}
+
+}
+
+export default Footer;
